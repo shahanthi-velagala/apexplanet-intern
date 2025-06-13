abf6a3724a3f082bf537b3c6ac11ef5f7dbff7a8
shahanthi-velagala <shahanthiv@gmail.com>
2025-06-13 10:58:54 +0530

    Add files via upload

---
+++ Task_1_web-basics/script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+  alert("Thank you for visiting CarStore! Explore our latest car collection.");
+}
+
+// Dynamic year for footer
+document.getElementById("year").textContent = new Date().getFullYear();
